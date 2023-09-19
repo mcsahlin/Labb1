6a3ea2a1a7d794e2572b693beb9f3b5001feec3c
mcsahlin <mcsahlin@outlook.com>
2023-09-19 07:27:45 +0200

    added failed search img and corresponding style

--- search.php
+++ search.php
@@ -1,42 +1,46 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : ?>
-<div id="primary" class="col-xs-12 col-md-9">
-  <h1><?= page(); ?></h1>
-  <?php while (have_posts()) : the_post(); ?>
-  <article>
-    <img src="<?= get_the_post_thumbnail_url(); ?>" />
-    <h2 class="title">
-      <a href="<?= the_permalink(); ?>"><?= the_title(); ?></a>
-    </h2>
-    <ul class="meta">
-      <li>
-        <i class="fa fa-calendar"></i>
-        <?= get_the_date(); ?>
-      </li>
-      <li>
-        <i class="fa fa-user"></i>
-        <?= get_the_author_posts_link(); ?>
-      </li>
-      <li>
-        <i class=" fa fa-tag"></i>
-        <?= the_category(", "); ?>
-      </li>
-    </ul>
-    <p><?= the_excerpt(); ?></p>
-  </article>
-  <?php endwhile; ?>
-  <nav class=" navigation pagination">
-    <h2 class="screen-reader-text">Inläggsnavigering</h2>
-    <a class="prev page-numbers" href="">Föregående</a>
-    <span class="page-numbers current">1</span>
-    <a class="page-numbers" href="">2</a>
-    <a class="next page-numbers" href="">Nästa</a>
-  </nav>
-</div>
+  <div id="primary" class="col-xs-12 col-md-9">
+    <h1><?= page(); ?></h1>
+    <?php while (have_posts()) : the_post(); ?>
+      <article>
+        <img src="<?= get_the_post_thumbnail_url(); ?>" />
+        <h2 class="title">
+          <a href="<?= the_permalink(); ?>"><?= the_title(); ?></a>
+        </h2>
+        <ul class="meta">
+          <li>
+            <i class="fa fa-calendar"></i>
+            <?= get_the_date(); ?>
+          </li>
+          <li>
+            <i class="fa fa-user"></i>
+            <?= get_the_author_posts_link(); ?>
+          </li>
+          <li>
+            <i class=" fa fa-tag"></i>
+            <?= the_category(", "); ?>
+          </li>
+        </ul>
+        <p><?= the_excerpt(); ?></p>
+      </article>
+    <?php endwhile; ?>
+    <nav class=" navigation pagination">
+      <h2 class="screen-reader-text">Inläggsnavigering</h2>
+      <a class="prev page-numbers" href="">Föregående</a>
+      <span class="page-numbers current">1</span>
+      <a class="page-numbers" href="">2</a>
+      <a class="next page-numbers" href="">Nästa</a>
+    </nav>
+  </div>
 <?php else : ?>
-<em>
-  <?php _e('Din sökning på "') ?><strong><?= get_search_query() ?></strong><?php _e('" gav inga resultat, men prova något annat :)', 'textdomain'); ?>
-</em>
+  <div class="search-fail">
+    <em class='search-fail-msg'>
+      <?php _e('Din sökning på "') ?><strong><?= get_search_query() ?></strong><?php _e('" gav inga resultat, men prova något annat :)', 'textdomain'); ?>
+    </em>
+    <img class="search-fail-img" src="http://localhost/wp-content/uploads/2023/09/Failure.jpg" alt="Failure" />
+  </div>
+
 <?php endif; ?>
 <?php get_footer(); ?>
